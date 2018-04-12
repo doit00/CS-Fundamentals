151f4470c6e09efd5462edd71b25840de86ae68c
JZ <JacekZ@nonexisting.com>
2018-04-12 16:32:46 +0200

    Singleton project added

---
+++ 14Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainConfigFile cf1 = MainConfigFile.Get();
+            MainConfigFile cf2 = MainConfigFile.Get();
+        }
+    }
+
+    class MainConfigFile
+    {
+        static readonly MainConfigFile mainConfig = new MainConfigFile();
+        private MainConfigFile()
+        {
+        }
+        public static MainConfigFile Get()
+        {
+
+            return mainConfig;
+        }
+
+
+    }
+
+}
