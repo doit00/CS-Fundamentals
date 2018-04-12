dd334d94fbeb0b77b1b100389df61a2283d63412
doit00 <36489572+doit00@users.noreply.github.com>
2018-04-12 23:02:28 +0200

    Preprocessor directives

---
+++ 15PreprocessorDirectives/Program.cs
@@ -0,0 +1,31 @@
+﻿#define DIAG
+using System;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //switch Debug to Release
+#if DEBUG
+            Console.WriteLine("debug is enabled");
+
+#else
+            Console.WriteLine("debug is not enabled");
+#endif
+
+#if DIAG
+#warning DIAG is defined
+            Console.WriteLine("diagnostic is enabled");
+#endif
+
+        }
+    }
+    class Data
+    {
+#pragma warning disable CS0649
+        public string format;
+    }
+}
